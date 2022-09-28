ec8a1ab9ad2c9a2b1e2db23178aed28bef6ee944
itm-mne <itm@it-minds.dk>
2022-09-28 11:34:04 +0200

    Added translation packages (i18next)

--- pelican-frontend/src/main.tsx
+++ pelican-frontend/src/main.tsx
@@ -1,10 +1,18 @@
+import i18next from 'i18next'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { I18nextProvider } from 'react-i18next'
 import App from './App'
 import './index.css'
 
+i18next.init({
+  interpolation: {escapeValue: false}
+});
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <I18nextProvider i18n={i18next}>
+      <App />
+    </I18nextProvider>
   </React.StrictMode>
 )
