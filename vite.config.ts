a8b4b6d8f69359889307b33b6f6bc4bce867be7f
s-bogelund <simbj94@gmail.com>
2022-09-29 14:53:50 +0200

    Added PWA using vite-plugin-pwa. Have to run 'npm i && npm run build' initially

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite';
+import { VitePWA } from 'vite-plugin-pwa';
 import react from '@vitejs/plugin-react';
-
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
+	// plugins: [react(), VitePWA({ registerType: 'autoUpdate', injectRegister: 'script' })],
+	plugins: [react(), VitePWA({ injectRegister: 'auto', registerType: 'autoUpdate' })],
 	server: {
 		port: 3000,
 	},
