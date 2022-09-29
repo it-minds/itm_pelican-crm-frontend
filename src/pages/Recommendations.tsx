f4dd61185422ccc232bfbe3d90ccf7d052ce8646
itm-mne <mne@it-minds.dk>
2022-09-29 14:10:42 +0200

    Added translation imports to pages with sample translation text

--- src/pages/Recommendations.tsx
+++ src/pages/Recommendations.tsx
@@ -1,11 +1,18 @@
-import { Grid } from '@mui/material'
+import { Grid, Typography } from '@mui/material'
 import React from 'react'
 import { pageContainer } from '../styles/containers'
+import { useTranslation } from 'react-i18next'
 
 const Recommendations = () => {
+  const {t, i18n} = useTranslation('common');
+
   return (
     <Grid sx={pageContainer} container>
-      <div>Recommendations</div>
+      <div>
+        <Typography color="text.primary">
+          {t("recommendations.pageTitle")}
+        </Typography>
+      </div>
     </Grid>
   )
 }
