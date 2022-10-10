74863b33413b3fa9a031dc595b9adfaed81b7adc
itm-mne <mne@it-minds.dk>
2022-10-10 10:35:29 +0200

    Created ProtectedRoute component

---
+++ src/components/common/ProtectedRoute.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Navigate, Outlet } from 'react-router-dom'
+
+const ProtectedRoute = ({user, redirectPath = '/' , children}: any) => {
+  if (!user) {
+    return <Navigate to={redirectPath} replace />
+  }
+
+  return (
+    children ? children : <Outlet />
+  )
+}
+
+export default ProtectedRoute
