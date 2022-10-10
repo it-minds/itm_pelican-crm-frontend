e931037201790ddd3571d80d44583176cef58b1e
itm-mne <mne@it-minds.dk>
2022-10-10 11:03:45 +0200

    Added 404 to routes, when no route matches

--- src/Content.tsx
+++ src/Content.tsx
@@ -3,6 +3,7 @@ import { Route, Routes, Navigate } from 'react-router-dom';
 import App from './App';
 import NavigationBar from './components/common/NavigationBar';
 import Contacts from './pages/Contacts';
+import NotFound404 from './pages/NotFound404';
 import Recommendations from './pages/Recommendations';
 import Suppliers from './pages/Suppliers';
 import WallOfClients from './pages/WallOfClients';
@@ -21,6 +22,7 @@ const Content = () => {
 				{/* Might need to use auth-wrapper instead and completely ignore "/". Then entry to the site should be through Googles authentication and after succesful login, access should be granted to the platform */}
 
 				<Route path="/" element={<App />} />
+        <Route path="*" element={<NotFound404 />} />
 			</Routes>
 		</>
 	);
