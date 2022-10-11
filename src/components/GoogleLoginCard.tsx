e4c8e2b268dcecfdd3087d547dd1f231abd2b04b
itm-mne <mne@it-minds.dk>
2022-10-11 15:46:22 +0200

    Added translation to GoogleLoginCard

--- src/components/GoogleLoginCard.tsx
+++ src/components/GoogleLoginCard.tsx
@@ -1,13 +1,21 @@
-import { Card, CardContent } from '@mui/material'
+import { Card, CardActions, CardContent, Typography } from '@mui/material'
 import React from 'react'
+import { useTranslation } from 'react-i18next';
 
 const GoogleLoginCard = () => {
+  const { t, i18n } = useTranslation();
+
   return (
     <>
-      <Card sx={{minWidth: 300}} variant="outlined">
+      <Card sx={{minWidth: 300, maxWidth: 400}} variant="outlined">
         <CardContent>
-          
+          <Typography>
+            {t("landingPage.pageTitle")}
+          </Typography>
         </CardContent>
+        <CardActions>
+          
+        </CardActions>
       </Card>
     </>
   )
