eb9c7e76415976681c6963498368dd44bb6702b4
itm-mne <mne@it-minds.dk>
2022-10-11 15:24:50 +0200

    Created GoogleLoginCard for landing page

---
+++ src/components/GoogleLoginCard.tsx
@@ -0,0 +1,16 @@
+import { Card, CardContent } from '@mui/material'
+import React from 'react'
+
+const GoogleLoginCard = () => {
+  return (
+    <>
+      <Card sx={{minWidth: 300}} variant="outlined">
+        <CardContent>
+          
+        </CardContent>
+      </Card>
+    </>
+  )
+}
+
+export default GoogleLoginCard
--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -4,17 +4,25 @@ import { useTranslation } from 'react-i18next'
 import GoogleLoginTest from '../components/GoogleLoginTest'
 import TranslationTitle from '../components/TranslationTitle'
 import { pageContainer } from '../styles/containers'
+import background from '../assets/s37med.jpg'
+import GoogleLoginCard from '../components/GoogleLoginCard'
 
 const LandingPage = () => {
   const {t, i18n} = useTranslation();
   
   return (
-      <Grid sx={pageContainer} container>
-        <Grid >
-          <TranslationTitle />
-          <GoogleLoginTest />
-        </Grid>
+    <Grid sx={pageContainer} 
+      container 
+      style={{backgroundImage:`url(${background})`, 
+        backgroundRepeat:"no-repeat", 
+        backgroundSize:"contain",
+        backgroundPosition: 'center'}}>
+      <Grid>
+        <TranslationTitle />
+        <GoogleLoginTest />
+        <GoogleLoginCard />
       </Grid>
+    </Grid>
   )
 }
 
@@ -22,7 +30,7 @@ export default LandingPage
 
 /**
  * Landing page:
- *  Ingen nav-bar
+ *  (Done gennem nav-wrappers) : Ingen nav-bar
  *  Baggrund (Logo eller lignende - bare noget lollern indtil vi har det)
  *  Div (Card?) til højre på siden der har en knap til at logge ind via google
  *    Skal også have knap til at gå til /clients, så man kan bruge siden normalt, da auth ikke virker
