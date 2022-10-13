89c4393edc3c398dfad3050906e7367bcf69a7fd
itm-mne <mne@it-minds.dk>
2022-10-13 11:57:39 +0200

    Initial code spaghetti - Created new underline component

--- src/components/common/UnderlinedV5.tsx
+++ src/components/common/UnderlinedV5.tsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import { Typography } from '@mui/material'
+import React, { FC } from 'react'
 
-const UnderlinedV5 = () => {
+/**
+ * Importer tema, så der kan tilgås (farver, spacing, etc.)
+ * Lav komponentets styling her inde i komponenten
+ * Lav det til en slags wrapper man bare kan smide rundt om et element
+ * Skal kun give sit direkte barn den specifikke styling!
+ */
+
+export type Props = {
+  children: React.ReactNode;
+}
+
+const UnderlinedV5: FC<Props> = ({
+  children
+}) => {
   return (
-    <div>UnderlinedV5</div>
+    <>
+      <Typography>
+        Her er underlined
+      </Typography>
+      {children}
+    </>
   )
 }
 
 export default UnderlinedV5
+/*
+  lave wrapper / style til komponenter der skal have understregning
+  Skal kun lave understregning på det direkte barn
+  skal "slide" ind når den renderes (cirka 0.3 sek) fra venstre mod højre
+  skal kunne vælge om der skal animation på (prop / state?)
+ */
--- src/pages/Recommendations.tsx
+++ src/pages/Recommendations.tsx
@@ -2,6 +2,7 @@ import { Grid, Typography } from '@mui/material'
 import React from 'react'
 import { pageContainer } from '../styles/containers'
 import { useTranslation } from 'react-i18next'
+import UnderlinedV5 from '../components/common/UnderlinedV5'
 
 const Recommendations = () => {
   const {t, i18n} = useTranslation();
@@ -9,9 +10,11 @@ const Recommendations = () => {
   return (
     <Grid sx={pageContainer} container>
       <div>
-        <Typography color="text.primary">
-          {t("recommendations.pageTitle")}
-        </Typography>
+        <UnderlinedV5>
+          <Typography color="text.primary">
+            {t("recommendations.pageTitle")}
+          </Typography>
+        </UnderlinedV5>
       </div>
     </Grid>
   )
