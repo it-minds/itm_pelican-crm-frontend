d46bf8e61f34c1359a218037d52b4be16f6d8a03
itm-mne <mne@it-minds.dk>
2022-10-17 11:25:14 +0200

    Added active state to underline component

--- src/components/common/Underlined.tsx
+++ src/components/common/Underlined.tsx
@@ -3,34 +3,38 @@ import React, { ReactNode } from 'react'
 
 export interface Props {
   children: ReactNode;
+  active?: boolean;
 }
 
 const borderWidth = 10;
 
 const underlineStyles = {
-  "&::before": {
-    backgroundColor: "secondary.main",
-    content: "''",
-    bottom: `${-borderWidth}%`,
-    height: `${borderWidth}%`,
-    width: "60%",
-    position: "absolute",
-    transition: ".2s ease-out width"
-  },
-  display: "inline-block",
-  position: "relative",
-  marginBottom: 8,
-  width: "fit-content"
+  root: {
+    "&::before": {
+      backgroundColor: "secondary.main",
+      content: "''",
+      bottom: `${-borderWidth}%`,
+      height: `${borderWidth}%`,
+      width: "60%",
+      position: "absolute",
+      transition: ".2s ease-out width"
+    },
+    display: "inline-block",
+    position: "relative",
+    marginBottom: 8,
+    width: "fit-content"
+  }
 }
 
 const Underlined = ({
   children,
+  active = true,
   ...props
 }: Props) => {
 
 
   return (
-    <Box sx={{...underlineStyles}}>
+    <Box sx={{...underlineStyles.root}}>
       {children}
     </Box>
   )
