4ac139fc86e1d208e7a5ddc50e47fa1d88994ff2
itm-mne <mne@it-minds.dk>
2022-10-19 10:59:28 +0200

    Added animation to underline component

--- src/components/common/Underlined.tsx
+++ src/components/common/Underlined.tsx
@@ -1,14 +1,16 @@
-import { Box } from '@mui/material';
+import { Box, styled } from '@mui/material';
 import React, { ReactNode } from 'react'
+import { SystemStyleObject, Theme } from '@mui/system';
 
 export interface Props {
   children: ReactNode;
   active?: boolean;
+  dynamic?: boolean;
 }
 
 const borderWidth = 10;
 
-const underlineStyles = {
+const underlineStyles: (theme: Theme) => SystemStyleObject<Theme> = (theme) => ({
   "&::before": {
     backgroundColor: "secondary.main",
     content: "''",
@@ -16,24 +18,44 @@ const underlineStyles = {
     height: `${borderWidth}%`,
     width: "60%",
     position: "absolute",
+    "@keyframes expand": {
+      from: {
+        width: "0%"
+      },
+      to: {
+        width: "60%"
+      }
+    },
+    animation: "expand .5s ease"
   },
   display: "inline-block",
   position: "relative",
   marginBottom: 3,
   width: "fit-content"
-}
+})
 
 const Underlined = ({
   children,
-  active = true,
+  active = false,
+  dynamic = false,
   ...props
 }: Props) => {
 
-  return (
-    <Box sx={{...(active && underlineStyles)}}>
-      {children}
-    </Box>
-  )
+  if (active) {
+    return (
+      // <Box sx={{...(active && underlineStyles)}}>    Comment kept as a reference to previous, cleaner code
+      <Box sx={underlineStyles}>
+        {children}
+      </Box>
+    )
+  }
+  else {
+    return (
+      <Box sx={underlineStyles}>
+        {children}
+      </Box>
+    )
+  }
 }
 
 export default Underlined
