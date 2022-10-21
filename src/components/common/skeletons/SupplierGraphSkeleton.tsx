aa9353e57bcc19037910cb602fac7a004070b796
itm-mne <mne@it-minds.dk>
2022-10-21 11:48:02 +0200

    Supplier graph skeleton refactor

--- src/components/common/skeletons/SupplierGraphSkeleton.tsx
+++ src/components/common/skeletons/SupplierGraphSkeleton.tsx
@@ -2,61 +2,57 @@ import { Skeleton } from '@mui/material'
 import { Stack } from '@mui/system'
 import React from 'react'
 
+const graphSkeletonAttr = [ 
+  {
+    width: "80%"
+  },
+  {
+    width: "70%"
+  },
+  {
+    width: "60%"
+  },
+  {
+    width: "55%"
+  },
+  {
+    width: "50%"
+  },
+  {
+    width: "45%"
+  },
+  {
+    width: "40%"
+  },
+  {
+    width: "25%"
+  },
+  {
+    width: "15%"
+  },
+  {
+    width: "12%"
+  },
+  {
+    width: "10%"
+  },
+  {
+    width: "8%"
+  },
+  {
+    width: "5%"
+  },
+];
+
 const SupplierGraphSkeleton = () => {
   return (
     <Stack spacing={1}>
-      <Stack spacing={1} direction="row">
-        <Skeleton variant="rectangular" height={20} width={20} />
-        <Skeleton variant="rounded" height={20} width={"80%"} />
+      {graphSkeletonAttr.map(skeleton => (
+        <Stack spacing={4} direction="row" alignItems="center">
+        <Skeleton variant="rectangular" height={12} width={"10%"} />
+        <Skeleton variant="rounded" height={20} width={skeleton.width} />
       </Stack>
-      <Stack spacing={1} direction="row">
-        <Skeleton variant="rectangular" height={20} width={20} />
-        <Skeleton variant="rounded" height={20} width={"70%"} />
-      </Stack>
-      <Stack spacing={1} direction="row">
-        <Skeleton variant="rectangular" height={20} width={20} />
-        <Skeleton variant="rounded" height={20} width={"60%"} />
-      </Stack>
-      <Stack spacing={1} direction="row">
-        <Skeleton variant="rectangular" height={20} width={20} />
-        <Skeleton variant="rounded" height={20} width={"55%"} />
-      </Stack>
-      <Stack spacing={1} direction="row">
-        <Skeleton variant="rectangular" height={20} width={20} />
-        <Skeleton variant="rounded" height={20} width={"50%"} />
-      </Stack>
-      <Stack spacing={1} direction="row">
-        <Skeleton variant="rectangular" height={20} width={20} />
-        <Skeleton variant="rounded" height={20} width={"45%"} />
-      </Stack>
-      <Stack spacing={1} direction="row">
-        <Skeleton variant="rectangular" height={20} width={20} />
-        <Skeleton variant="rounded" height={20} width={"40%"} />
-      </Stack>
-      <Stack spacing={1} direction="row">
-        <Skeleton variant="rectangular" height={20} width={20} />
-        <Skeleton variant="rounded" height={20} width={"25%"} />
-      </Stack>
-      <Stack spacing={1} direction="row">
-        <Skeleton variant="rectangular" height={20} width={20} />
-        <Skeleton variant="rounded" height={20} width={"15%"} />
-      </Stack>
-      <Stack spacing={1} direction="row">
-        <Skeleton variant="rectangular" height={20} width={20} />
-        <Skeleton variant="rounded" height={20} width={"12%"} />
-      </Stack>
-      <Stack spacing={1} direction="row">
-        <Skeleton variant="rectangular" height={20} width={20} />
-        <Skeleton variant="rounded" height={20} width={"10%"} />
-      </Stack>
-      <Stack spacing={1} direction="row">
-        <Skeleton variant="rectangular" height={20} width={20} />
-        <Skeleton variant="rounded" height={20} width={"8%"} />
-      </Stack>
-      <Stack spacing={1} direction="row">
-        <Skeleton variant="rectangular" height={20} width={20} />
-        <Skeleton variant="rounded" height={20} width={"5%"} />
-      </Stack>
+      ))}
     </Stack>
   )
 }
