7154ad6c550fea0ebd74e94c0ac67c7a98067285
s-bogelund <simbj94@gmail.com>
2022-10-21 11:05:48 +0200

    More cleanup stuff

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,8 @@
-import { createContext, useContext, useEffect, useState } from 'react';
-import reactLogo from './assets/react.svg';
 import './App.css';
-import TranslationTitle from './components/TranslationTitle';
-import { Button, Grid, Typography } from '@mui/material';
-import { pageContainer } from './styles/containers';
-import { Box } from '@mui/system';
-import NavigationBar from './components/common/NavigationBar';
-import AppThemeProvider, { ThemeContext } from './ThemeContext';
-import { useTheme } from '@mui/material';
-import GoogleLoginTest from './components/GoogleLoginButton';
+
 import LandingPage from './pages/LandingPage';
 
-/*
-  At some point this page should be made into a landing page for the application
-  Landing page should redirect to /clients if a user is logged in or prompt user to log in, if not logged in
-*/
-
 function App() {
-	const themes = useTheme();
-
-	function handleCallbackResponse(response: { credential: any }) {
-		console.log(`Encoded JWT ID token: ${response.credential}`);
-	}
-
-	useEffect(() => {}, []);
-
 	return (
 		<>
 			<LandingPage />
