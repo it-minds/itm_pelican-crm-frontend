34598f9a6608cda1d630c4c7a46abcecebdf78a8
itm-mne <mne@it-minds.dk>
2022-10-25 15:09:21 +0200

    prettier and linter setup

--- src/components/common/ImageContainer.tsx
+++ src/components/common/ImageContainer.tsx
@@ -1,40 +1,35 @@
 import { Box } from '@mui/system';
 import { styled } from '@mui/system';
-import React, { FC, ReactNode } from 'react'
+import React, { FC, ReactNode } from 'react';
 
 export type Props = {
-  children: ReactNode;
-  className?: string;
-} & StyleProps
+	children: ReactNode;
+	className?: string;
+} & StyleProps;
 
 type StyleProps = {
-  imageSize?: string | number | null;
+	imageSize?: string | number | null;
 };
 
-const StyledImageContainer = styled(Box)<StyleProps>(({ theme }) => 
-  ({
-    imageSize = null
-  }: StyleProps) => ({
-    width: imageSize != null ? imageSize : "auto",
-    height: imageSize != null ? imageSize : "100%",
-    objectFit: "contain",
-}));
+const StyledImageContainer = styled(Box)<StyleProps>(
+	({ theme }) =>
+		({ imageSize = null }: StyleProps) => ({
+			width: imageSize != null ? imageSize : 'auto',
+			height: imageSize != null ? imageSize : '100%',
+			objectFit: 'contain',
+		})
+);
 
-const ImageContainer: FC<Props> = ({children, ...styleProps }:Props) => {
-  const { imageSize } = styleProps;
+const ImageContainer: FC<Props> = ({ children, ...styleProps }: Props) => {
+	const { imageSize } = styleProps;
 
-  // Hvis imageSize er sat, så skal boxen have en bestemt størrelse
-  // - ellers skal den som default bare fylde pladsen ud
+	// Hvis imageSize er sat, så skal boxen have en bestemt størrelse
+	// - ellers skal den som default bare fylde pladsen ud
 
-  return (
-    <StyledImageContainer
-      imageSize={imageSize}
-    >
-    </StyledImageContainer>
-  )
-}
+	return <StyledImageContainer imageSize={imageSize} />;
+};
 
-export default ImageContainer
+export default ImageContainer;
 
 // const StyledPageContainer = styled(Grid)<Props>(({ theme }) => (props: Props) => ({
 //   backgroundColor: "background.default",
