8cb8da4d00860f4d137a89a30d897543c0a4eecb
s-bogelund <simbj94@gmail.com>
2022-10-26 22:46:32 +0200

    Small improvements and fixes

--- src/components/summaries/ClientInfoSummary.tsx
+++ src/components/summaries/ClientInfoSummary.tsx
@@ -1,7 +1,7 @@
 import LocationCityIcon from '@mui/icons-material/LocationCity';
 import WebIcon from '@mui/icons-material/Web';
 import { Box, Grid, SxProps, Tooltip, Typography, useMediaQuery, useTheme } from '@mui/material';
-import React, { FC, ReactElement, useEffect, useRef, useState } from 'react';
+import React, { FC, useEffect, useRef, useState } from 'react';
 /**
  * ! Needs the following:
  * 	* Responsiveness (useMediaQuery)
@@ -21,9 +21,7 @@ type Props = {
 const ClientInfoSummary: FC<Props> = ({ children, title, city, url, width, address }) => {
 	const theme = useTheme();
 	const titleRef = useRef<HTMLDivElement>(null);
-	const mediaQuery = useMediaQuery(theme.breakpoints.down('md'));
-
-	console.log('titleRef', titleRef);
+	const smallScreen = useMediaQuery(theme.breakpoints.down('sm'));
 
 	const [iconColor, setIconColor] = useState(theme.palette.primary.main);
 
@@ -44,7 +42,7 @@ const ClientInfoSummary: FC<Props> = ({ children, title, city, url, width, addre
 	return (
 		<Grid width={width} container direction="column" paddingX="0.4rem" gap="2px">
 			<Grid item width="100%">
-				<Tooltip sx={{ width: 'fit-content' }} ref={titleRef} title={title} placement="top-start">
+				<Tooltip ref={titleRef} title={smallScreen ? title : ''} placement="top-start">
 					<Typography noWrap variant="h6">
 						{title}
 					</Typography>
@@ -53,7 +51,7 @@ const ClientInfoSummary: FC<Props> = ({ children, title, city, url, width, addre
 			<Grid sx={{ justifyContent: 'space-between', ml: '-1px' }} direction="row" container item>
 				<Tooltip title={address || ''} placement="bottom-start">
 					<Box display="flex" justifyContent="center" alignItems="end" gap="2px">
-						<LocationCityIcon sx={{ color: iconColor }} />
+						<LocationCityIcon sx={{ color: iconColor, mb: '1px' }} />
 						<Typography variant="subtitle2">{city || 'unknown'}</Typography>
 					</Box>
 				</Tooltip>
@@ -66,7 +64,7 @@ const ClientInfoSummary: FC<Props> = ({ children, title, city, url, width, addre
 						sx={{ cursor: 'pointer' }}
 						onClick={() => window.open(`https://www.${url}`, '_blank')}
 					>
-						<WebIcon fontSize="small" sx={{ color: iconColor }} />
+						<WebIcon fontSize="small" sx={{ color: iconColor, mb: '1px' }} />
 						<Typography variant="subtitle2">{urlToDisplay()}</Typography>
 					</Box>
 				</Tooltip>
