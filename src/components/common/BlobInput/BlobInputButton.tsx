0e14ac09fe470387c0eef1de20bfeced6f854745
s-bogelund <simbj94@gmail.com>
2022-11-08 09:18:18 +0100

    Popover now shows with any child component with clickaway listener

--- src/components/common/BlobInput/BlobInputButton.tsx
+++ src/components/common/BlobInput/BlobInputButton.tsx
@@ -1,15 +1,36 @@
-import { Box, ButtonBase, IconButton, styled, Typography } from '@mui/material';
-import React, { FC } from 'react';
+import {
+	Box,
+	ButtonBase,
+	ButtonProps,
+	IconButton,
+	Popover,
+	styled,
+	Typography,
+} from '@mui/material';
+import { Button as MuiButton } from '@mui/material';
+import React, { FC, useState } from 'react';
 import CloseIcon from '@mui/icons-material/Close';
 import Button from '../Button';
+import ClientInfoSummary from '../../summaries/ClientInfoSummary';
 
-type BlobInputProps = { active?: boolean; onClick: () => void; clearClick?: () => void };
+type BlobInputProps = {
+	active?: boolean;
+	// onClick: () => void;
+	clearClick?: () => void;
+	children?: React.ReactNode;
+} & ButtonProps;
 
-const BlobInputButton: FC<BlobInputProps> = ({ active, onClick, clearClick }) => {
+const BlobInputButton: FC<BlobInputProps> = ({ active, clearClick, children, onClick }) => {
+	const [showChildren, setShowChildren] = useState(false);
+	const [anchorEl, setAnchorEl] = useState<HTMLButtonElement | null>(null);
 	const xClicked = (event: any) => {
 		event.stopPropagation();
-		event.preventDefault();
 		clearClick && clearClick();
+	};
+
+	const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+		setAnchorEl(event.currentTarget);
+		setShowChildren(true);
 	};
 
 	return (
@@ -23,7 +44,7 @@ const BlobInputButton: FC<BlobInputProps> = ({ active, onClick, clearClick }) =>
 				}}
 				noPad
 				btnType={active ? 'default' : 'outlined'}
-				onClick={onClick}
+				onClick={event => handleClick(event)}
 			>
 				{active && (
 					<ButtonBase sx={{ borderRadius: '25px' }} onMouseDown={event => xClicked(event)}>
@@ -36,6 +57,24 @@ const BlobInputButton: FC<BlobInputProps> = ({ active, onClick, clearClick }) =>
 					</Typography>
 				</Box>
 			</Button>
+			<Popover
+				open={showChildren}
+				anchorEl={anchorEl}
+				onClose={() => setShowChildren(false)}
+				sx={{ borderRadius: '16px' }}
+			>
+				<Box>
+					{children || (
+						<ClientInfoSummary
+							city="Aarhus"
+							title="Djurs Sommerland"
+							address="Holdkæftvej 1"
+							sx={{ borderRadius: '26px' }}
+							url="https://www.djurssommerland.dk/"
+						/>
+					)}
+				</Box>
+			</Popover>
 		</div>
 	);
 };
