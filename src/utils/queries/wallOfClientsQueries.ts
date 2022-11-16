b07ae2bcdb49323354b4387f7246eb44ef9f7f08
itm-mne <mne@it-minds.dk>
2022-11-16 15:33:59 +0100

    Extended WoC query

--- src/utils/queries/wallOfClientsQueries.ts
+++ src/utils/queries/wallOfClientsQueries.ts
@@ -4,45 +4,62 @@ export const GET_CLIENTS = gql`
 	query GetClients {
 		clients {
 			nodes {
-				id
-				name
-				officeLocation
-				pictureUrl
-				website
-				deals {
-					id
-					dealStatus
-					endDate
-					lastUpdatedAt
-					createdAt
-					dealContacts {
-						id
-						contact {
-							firstname
-							lastname
-							phoneNumber
-							email
-							jobTitle
-							linkedInUrl
-						}
-					}
-					accountManagerDeals {
-						accountManager {
-							firstName
-							lastName
-							id
-							email
-							phoneNumber
-							supplier {
-								name
-								officeLocations {
-									cityName
-								}
-							}
-						}
+				clientContacts {
+					contact {
+						firstname
+						lastname
 					}
 				}
 			}
 		}
 	}
 `;
+
+// TODO: Remove above test query in favor for the one below
+// export const GET_CLIENTS = gql`
+// 	query GetClients {
+// 		clients {
+// 			nodes {
+// 				id
+// 				name
+// 				officeLocation
+// 				pictureUrl
+// 				website
+// 				deals {
+// 					id
+// 					dealStatus
+// 					endDate
+// 					lastUpdatedAt
+// 					createdAt
+// 					dealContacts {
+// 						id
+// 						contact {
+// 							firstname
+// 							lastname
+// 							phoneNumber
+// 							email
+// 							jobTitle
+// 							linkedInUrl
+// 							isActive
+// 						}
+// 					}
+// 					accountManagerDeals {
+// 						accountManager {
+// 							firstName
+// 							lastName
+// 							id
+// 							email
+// 							phoneNumber
+// 							supplier {
+// 								name
+// 								officeLocations {
+// 									cityName
+// 								}
+// 							}
+// 						}
+// 					}
+// 				}
+// 			}
+// 		}
+// 	}
+// `;
