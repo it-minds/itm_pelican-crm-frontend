d5712284fe1908aa7a5d4d273572b0df24ff11f2
itm-mne <mne@it-minds.dk>
2022-11-25 09:46:05 +0100

    Added supplier, contact and deal type

--- src/utils/queryTypes.ts
+++ src/utils/queryTypes.ts
@@ -1,7 +1,19 @@
-import { Client } from '../gql/graphql';
+import { Client, Contact, Deal, Supplier } from '../gql/graphql';
 
 export type FetchedClient = Pick<
 	Client,
 	'id' | 'name' | 'officeLocation' | 'website' | 'clientContacts'
 >;
 // TODO: Should be renamed to ClientSummary after ClientInfoSummary has been refactored
+
+export type FetchedSupplier = Pick<Supplier, 'id' | 'name' | 'pictureUrl' | 'officeLocations'>;
+
+export type FetchedContact = Pick<
+	Contact,
+	'id' | 'firstname' | 'lastname' | 'email' | 'phoneNumber' | 'dealContacts'
+>;
+
+export type FetchedDeal = Pick<
+	Deal,
+	'id' | 'dealStatus' | 'startDate' | 'endDate' | 'accountManagerDeals'
+>;
