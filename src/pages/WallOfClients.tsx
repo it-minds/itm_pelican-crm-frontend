6032b4022e0b4843d2f942563636e52d70f7f7f7
itm-mne <mne@it-minds.dk>
2022-11-28 15:47:16 +0100

    Corrected type of ClientListItem type

--- src/pages/WallOfClients.tsx
+++ src/pages/WallOfClients.tsx
@@ -129,7 +129,7 @@ const WallOfClients = () => {
 					}}
 				>
 					{data?.clients?.nodes?.map(client => (
-						<ClientListItem clientListItem={client} />
+						<ClientListItem client={client} />
 					))}
 				</Box>
 			)}
