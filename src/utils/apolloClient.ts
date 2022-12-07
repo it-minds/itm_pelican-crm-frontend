5fcc3b1568beafa2e416dd95109e6ce5e6f34583
itm-mne <mne@it-minds.dk>
2022-12-07 11:23:44 +0100

    Added apollo fieldPolicy to query

--- src/utils/apolloClient.ts
+++ src/utils/apolloClient.ts
@@ -2,5 +2,18 @@ import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 export const client = new ApolloClient({
 	uri: 'https://pelican-linux-web-app.azurewebsites.net/graphql/',
-	cache: new InMemoryCache(),
+	cache: new InMemoryCache({
+		typePolicies: {
+			Query: {
+				fields: {
+					getFilteredClients: {
+						keyArgs: false,
+						merge(existing = [], incoming) {
+							return [...existing, ...incoming];
+						},
+					},
+				},
+			},
+		},
+	}),
 });
