31ae8c69e4b37d8758341a801f515764f3e878f2
itm-mne <mne@it-minds.dk>
2023-01-10 16:00:35 +0100

    Added test auth workflow

--- src/Content.tsx
+++ src/Content.tsx
@@ -5,7 +5,7 @@ import { Route, Routes } from 'react-router-dom';
 import PageContainer from './components/common/PageContainer';
 import WithNav from './components/navWrapper/WithNav';
 import WithoutNav from './components/navWrapper/WithoutNav';
-import UserStore from './contexts/UserStore';
+import UserStore, { ActiveUser } from './contexts/UserStore';
 import Contacts from './pages/Contacts';
 import LoginPage from './pages/Login';
 import NotFound404 from './pages/NotFound404';
@@ -16,23 +16,17 @@ import WallOfClients from './pages/WallOfClients';
 const Content = () => {
 	const currentUser = UserStore.useLoginState();
 
-	// TODO: Is is beneficial to move the routes into App instead for a better flow through the app?
-	// TODO: Right now Content is nothing but the routes and it feels more intuitive to have App.tsx as the entry point instead of Content.tsx
-	// TODO: Basically Content feels like an unnecessary component.
+	//* Test variables and objects for preliminary auth
+	const testCredetials: ActiveUser = {
+		id: 1,
+		isLoggedIn: true,
+		name: 'Mads',
+		role: 'admin',
+	};
 
-	/**
-	 * Arbejdsopgaver:
-	 * Tjek om der ligger en token (JWT) i local storage
-	 * 		Hvis der gør, så valider denne token med backend
-	 * 		Hvis den er valid, så kør logikken for at dekode token og sætte nuværende bruger via "setLoginState" (fra UserStore.tsx)
-	 * Hvis ikke der er en JWT i local storage, så skal der laves en query til backend for at få en ny
-	 * 		Denne JWT skal så dekodes for at finde ud af rollen, navn, om de er logget ind, etc ...
-	 * 		Når JWT er dekodet, så bruges dennes information til at sætte brugeren via setLoginState (fra UserStore.tsx)
-	 * 		Herefter sættes currentUser til den nuværende bruger via "useLoginState" (fra UserStore.tsx)
-	 * Hver gang en route tilgås skal JWT valideres i backend
-	 * 		Der skal sandsynligvis opsættes i backend, så token refresher hver gang den bruges
-	 * 		Der sættes en bestemt levetid på token, så den udløber efter X timers inaktivitet
-	 */
+	checkForJWT(testCredetials);
+
+	localStorage.setItem('user', JSON.stringify(testCredetials));
 
 	return (
 		<>
@@ -69,4 +63,35 @@ const Content = () => {
 	);
 };
 
+// Preliminary auth with JWT in localstorage
+function checkForJWT(credentials: ActiveUser) {
+	let user: ActiveUser = {} as ActiveUser;
+	if (!localStorage.getItem('user')) {
+		localStorage.setItem('user', JSON.stringify(credentials)); // TODO: Replace with query to backend that creates new token
+		user = JSON.parse(localStorage.getItem('user') || '{}');
+	} else {
+		user = JSON.parse(localStorage.getItem('user') || '{}');
+	}
+	UserStore.setActiveUser(user);
+	//TODO: Probably need some kind of error handling
+}
+
 export default Content;
+
+// TODO: Is is beneficial to move the routes into App instead for a better flow through the app?
+// TODO: Right now Content is nothing but the routes and it feels more intuitive to have App.tsx as the entry point instead of Content.tsx
+// TODO: Basically Content feels like an unnecessary component.
+
+/**
+ * Arbejdsopgaver:
+ * Tjek om der ligger en token (JWT) i local storage
+ * 		Hvis der gør, så valider denne token med backend
+ * 		Hvis den er valid, så kør logikken for at dekode token og sætte nuværende bruger via "setLoginState" (fra UserStore.tsx)
+ * Hvis ikke der er en JWT i local storage, så skal der laves en query til backend for at få en ny
+ * 		Denne JWT skal så dekodes for at finde ud af rollen, navn, om de er logget ind, etc ...
+ * 		Når JWT er dekodet, så bruges dennes information til at sætte brugeren via setLoginState (fra UserStore.tsx)
+ * 		Herefter sættes currentUser til den nuværende bruger via "useLoginState" (fra UserStore.tsx)
+ * Hver gang en route tilgås skal JWT valideres i backend
+ * 		Der skal sandsynligvis opsættes i backend, så token refresher hver gang den bruges
+ * 		Der sættes en bestemt levetid på token, så den udløber efter X timers inaktivitet
+ */
